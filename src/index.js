7b8d0eadbdfa64e456882241d76558c57d2d8bf0
Darshan Baid <hey@darshanbaid.com>
2020-08-30 00:12:56 +0530

    added tile engine

--- src/index.js
+++ src/index.js
@@ -1,117 +1,48 @@
 import "./styles.css";
 
-import { init, Sprite, GameLoop, Pool, collides } from "kontra";
+import {
+  init,
+  Sprite,
+  GameLoop,
+  load,
+  TileEngine,
+  dataAssets,
+  SpriteSheet
+} from "kontra";
 
 let { canvas } = init();
 
-let sky = Sprite({
-  x: 20,
-  y: 40,
-  height: 40,
-  width: 600 - 20 * 2,
-  color: "white"
-});
+load("src/m.png", "src/m2.json", "src/colored_packed.json").then((assets) => {
+  let tileEngine = TileEngine(dataAssets["src/m2"]);
 
-let bricks = Pool({
-  create: Sprite
-});
-
-bricks.get({
-  x: 100,
-  y: 400,
-  color: "yellow",
-  width: 60,
-  height: 40,
-  dx: 2
-});
-
-bricks.get({
-  x: 100 + (60 + 60) * 1,
-  y: 400,
-  color: "yellow",
-  width: 60,
-  height: 40,
-  dx: 2
-});
-
-bricks.get({
-  x: 100 + (60 + 60) * 2,
-  y: 400,
-  color: "yellow",
-  width: 60,
-  height: 40,
-  dx: 2
-});
-
-bricks.get({
-  x: 100 + (60 + 60) * 3,
-  y: 400,
-  color: "yellow",
-  width: 60,
-  height: 40,
-  dx: 2
-});
-
-bricks.get({
-  x: 100 + (60 + 60) * 4,
-  y: 400,
-  color: "yellow",
-  width: 60,
-  height: 40,
-  dx: 2
-});
-
-let ball = Sprite({
-  x: 300,
-  y: 100,
-  color: "red",
-  radius: 20,
-  dy: 2,
-  height: 30,
-  width: 30
-
-  // render: function () {
-  //   this.context.fillStyle = this.color;
-  //   this.context.beginPath();
-  //   this.context.arc(0, 0, this.radius, 0, 2 * Math.PI);
-  //   this.context.fill();
-  // }
-});
-
-let loop = GameLoop({
-  // create the main game loop
-  update: function () {
-    // update the game state
-
-    let reverseBall = false;
-    if (collides(ball, sky)) {
-      reverseBall = true;
-    }
-
-    for (let brick of bricks.objects) {
-      if (brick.x > canvas.width) {
-        brick.x = -brick.width;
-      } else if (collides(ball, brick)) {
-        reverseBall = true;
+  // let spriteSheet = SpriteSheet(dataAssets["src/colored_packed"]);
+  let hero = Sprite({
+    x: 100,
+    y: 100,
+    height: 16,
+    width: 16,
+    color: "red",
+    dx: 16,
+    update: function () {
+      this.x += this.dx;
+      if (this.x > canvas.width || this.x <= 0) {
+        this.dx = -this.dx;
       }
     }
-
-    if (ball.y > canvas.height || ball.y <= 0) {
-      reverseBall = true;
+  });
+  tileEngine.addObject(hero);
+  let loop = GameLoop({
+    // create the main game loop
+    update: function () {
+      hero.update();
+      // update the game state
+    },
+    render: function () {
+      // render the game state
+      hero.render();
+      tileEngine.render();
     }
+  });
 
-    if (reverseBall) {
-      ball.dy = -ball.dy;
-    }
-    bricks.update();
-    ball.update();
-  },
-  render: function () {
-    // render the game state
-    bricks.render();
-    sky.render();
-    ball.render();
-  }
+  loop.start(); // start the game
 });
-
-loop.start(); // start the game
