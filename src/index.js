5c4f563b20221eb8168c8dcba8b2806d2fe24100
Darshan Baid <hey@darshanbaid.com>
2020-08-29 21:51:31 +0530

    more boxes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
-import { init, Sprite, GameLoop, Pool } from "kontra";
+import { init, Sprite, GameLoop, Pool, collides } from "kontra";
 
 let { canvas } = init();
 
@@ -26,7 +26,7 @@ bricks.get({
 });
 
 bricks.get({
-  x: 260,
+  x: 100 + (60 + 60) * 1,
   y: 400,
   color: "yellow",
   width: 60,
@@ -34,24 +34,83 @@ bricks.get({
   dx: 2
 });
 
+bricks.get({
+  x: 100 + (60 + 60) * 2,
+  y: 400,
+  color: "yellow",
+  width: 60,
+  height: 40,
+  dx: 2
+});
+
+bricks.get({
+  x: 100 + (60 + 60) * 3,
+  y: 400,
+  color: "yellow",
+  width: 60,
+  height: 40,
+  dx: 2
+});
+
+bricks.get({
+  x: 100 + (60 + 60) * 4,
+  y: 400,
+  color: "yellow",
+  width: 60,
+  height: 40,
+  dx: 2
+});
+
+let ball = Sprite({
+  x: 300,
+  y: 100,
+  color: "red",
+  radius: 20,
+  dy: 2,
+  height: 30,
+  width: 30
+
+  // render: function () {
+  //   this.context.fillStyle = this.color;
+  //   this.context.beginPath();
+  //   this.context.arc(0, 0, this.radius, 0, 2 * Math.PI);
+  //   this.context.fill();
+  // }
+});
+
 let loop = GameLoop({
   // create the main game loop
   update: function () {
     // update the game state
-    bricks.update();
 
-    // wrap the sprites position when it reaches
-    // the edge of the screen
+    let reverseBall = false;
+    if (collides(ball, sky)) {
+      reverseBall = true;
+    }
+
     for (let brick of bricks.objects) {
       if (brick.x > canvas.width) {
         brick.x = -brick.width;
+      } else if (collides(ball, brick)) {
+        reverseBall = true;
       }
     }
+
+    if (ball.y > canvas.height || ball.y <= 0) {
+      reverseBall = true;
+    }
+
+    if (reverseBall) {
+      ball.dy = -ball.dy;
+    }
+    bricks.update();
+    ball.update();
   },
   render: function () {
     // render the game state
     bricks.render();
     sky.render();
+    ball.render();
   }
 });
 
